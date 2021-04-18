3628b657c60b38507daac24e552e9471a6a6f3b3
Cristobal <correodecristobal@gmail.com>
2021-04-18 11:42:43 -0600

    first challenge complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // let x: u8 = 255;
+    let x = 10.2;
+    println!("Hello world!");
+    println!("x is {}", x);
+
+    let a = 13;
+    let b = 2.3;
+    let c: f32 = 120.0;
+
+    let average = (a as f64 + b + c as f64) / 3 as f64;
+
+    assert_eq!(average, 45.1);
 }
