c760662914a8aa3a24cb16289d4077688b7eb9a9
Cristobal <correodecristobal@gmail.com>
2021-05-07 22:59:08 -0600

    challenge max, min, mean

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 fn main() {
-    let celsius_temp = 23.0;
-    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let max: i32;
+    let min: i32;
+    let mean: f64;
 
-    assert_eq!(fahrenheit_temp, 73.4);
-    println!("Test passed!");
-}
+    let mut temp_max = 0;
+    let mut temp_min = 0;
+    let mut sum = 0;
 
-fn celsius_to_fahrenheit(celsius_temp: f64) -> f64 {
-    1.8 * celsius_temp + 32.0
+    for (index, num) in numbers.iter().enumerate() {
+        println!("num is {}", num);
+        temp_max = if index == 0  {*num} else if num > &temp_max {*num} else {temp_max};
+        temp_min = if index == 0 {*num} else if num < &temp_min {*num} else {temp_min};
+        sum = sum + num;
+    }
+    max = temp_max;
+    min = temp_min;
+    mean = sum as f64 / numbers.len() as f64;
+
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Tests passed!");
 }
